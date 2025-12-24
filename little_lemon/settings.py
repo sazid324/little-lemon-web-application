97f0b0e207313655dff881004ba83fee97813413
Sazid Ahsan <sazidahsan423@gmail.com>
2025-12-24 20:35:09 +0600

    Replace mysqlclient with PyMySQL for Windows compatibility

--- little_lemon/settings.py
+++ little_lemon/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+
+# Ensure PyMySQL is used for MySQL connections
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
